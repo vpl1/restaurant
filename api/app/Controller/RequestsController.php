13f9ad270b466421bea75e143a0df1673faa62ad
vpl1 <vanluan.sphue@gmail.com>
2016-07-29 09:24:20 +0700

    edit postFeeBack method

--- api/app/Controller/RequestsController.php
+++ api/app/Controller/RequestsController.php
@@ -19,12 +19,13 @@ class RequestsController extends AppController {
 	        $this->LoadModel("FoodReview");
 	        $this->LoadModel("FeedBack");
 	        $this->LoadModel("Restaurant");
+	        $this->LoadModel("User");
 	    }
 	    // Start vpLuan
 	    public function getDeliveryProcess()
 	    {
 	    	$restaurantId = $this->request->query["restaurantId"];
-	    	if (!empty(trim((string)$restaurantId))  && $this->Restaurant->read(null,$restaurantId) != null){
+	    	if (!empty(trim((string)$restaurantId))  && $this->Restaurant->read(null,$restaurantId)){
 	            $queryResult = $this->RestaurantInfo->getDelivery($restaurantId);
 	            if ($queryResult !=null){
 	                $result = array("error"=>array('code'=>0,'message'=>'Connect successfully'),"contents"=>$queryResult);
@@ -42,19 +43,35 @@ class RequestsController extends AppController {
 
 	   	public function postFeedback(){
         	$repuestData = (array)$this->request->input('json_decode');
-            $feedbackData = (array)$repuestData['feedback'];        	
-           	$data = array(
-           		'user_id' => $feedbackData['userId'],
-           		'restaurant_id' => $feedbackData['restaurantId'],
-           		'email' => $feedbackData['email'],
-           		'first_name' => $feedbackData['firstmame'],
-           		'last_name' => $feedbackData['lastname'],
-           		'phone' => $feedbackData['phone'],
-           		'content' => $feedbackData['content'],
-           	);
-           	$resultquery = $this->FeedBack->saveFeeBack($data);   
-           	if($resultquery != null){           		
-	        	echo json_encode($resultquery);
+            $feedbackData = (array)$repuestData['feedback']; 
+            $user_id = null; $restaurant_id = null; $email = null; $first_name = null; $last_name = null; $phone = null; $content = null;
+            $status = true;
+           	empty(trim((string)$feedbackData['userId'])) ? $status = false : $user_id = $feedbackData['userId'];
+           	empty(trim((string)$feedbackData['restaurantId'])) ? $status = false : $restaurant_id = $feedbackData['restaurantId'];
+           	empty(trim((string)$feedbackData['email'])) ? $status = false : $email = $feedbackData['email'];
+           	empty(trim((string)$feedbackData['firstName'])) ? $status = false : $first_name = $feedbackData['firstName'];
+           	empty(trim((string)$feedbackData['lastName'])) ? $status = false : $last_name = $feedbackData['lastName'];
+           	empty(trim((string)$feedbackData['phone'])) ? $status = false : $phone = $feedbackData['phone'];
+           	empty(trim((string)$feedbackData['content'])) ? $status = false : $content = $feedbackData['content'];
+     
+           	if(count($status) && $this->Restaurant->read(null,$restaurant_id) != null && $this->User->read(null,$user_id) != null){
+           		$data = array(
+           		'user_id' => $user_id,
+           		'restaurant_id' => $restaurant_id,
+           		'email' => $email,
+           		'first_name' => $first_name,
+           		'last_name' => $last_name,
+           		'phone' => $phone,
+           		'content' => $content,
+	           	);
+	           	$resultquery = $this->FeedBack->saveFeeBack($data);   
+	           	if($resultquery != null){           		
+		        	echo json_encode($resultquery);
+	           	}
+           	}
+           	else{
+           		$result = array("error"=>array('code'=>0,'message'=>'Connect failed'));
+				echo json_encode($result);
            	}
            	die;        	        
         }
